ae4c5bff6d3422aedbca28f3656ea9259b8e6ddb
Leonardo Kremer <devleonardok@gmail.com>
2024-10-09 15:43:25 -0300

    remove unnecessary files, add styled-components and some styles

---
+++ src/components/Container/index.js
@@ -0,0 +1,13 @@
+import * as S from "./styles";
+
+export const Container = () => {
+  return (
+    <>
+      <S.Container>
+        <S.Section>
+          <S.Text>Olá TESTE</S.Text>
+        </S.Section>
+      </S.Container>
+    </>
+  );
+};
